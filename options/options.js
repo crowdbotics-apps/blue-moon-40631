9bc4dae7b50b9e62de5ba24fac36bb11cec9579d
Crowdbotics <team@crowdbotics.com>
2023-05-11 19:28:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-moon-40631",
+  url: "https://blue-moon-40631.botics.co",
+  api: "https://blue-moon-40631.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
